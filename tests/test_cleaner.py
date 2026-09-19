7e2e979240c80edf4d87919ddc3c0e2f27d080b0
md-4n4s <m.d.4n4s@gmail.com>
2026-09-19 21:31:26 +0500

    add cleaner tests

---
+++ tests/test_cleaner.py
@@ -0,0 +1,60 @@
+from src.preprocessing.cleaner import *
+
+
+def test_standardize_column_names() -> None:
+    original = pd.DataFrame(
+        {
+            "  Age": [20, 30, 40],
+            "student name": ["Alice", "Bob", "Charlie"],
+            "Marks  ": [5, 6, 7],
+        }
+    )
+
+    expected = pd.DataFrame(
+        {
+            "Age": [20, 30, 40],
+            "student_name": ["Alice", "Bob", "Charlie"],
+            "Marks": [5, 6, 7],
+        }
+    )
+
+    result = standardize_column_names(original)
+
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_drop_unnecessary_columns() -> None:
+
+    original = pd.DataFrame(
+        {"Age": [20, 30, 40], "Name": ["Alice", "Bob", "Charlie"], "Marks": [5, 6, 7]}
+    )
+
+    expected = pd.DataFrame({"Age": [20, 30, 40]})
+
+    result = drop_unnecessary_columns(original, ["Name", "Marks"])
+
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_drop_duplicates() -> None:
+
+    original = pd.DataFrame(
+        {"Age": [20, 30, 20], "Name": ["Alice", "Bob", "Alice"], "Marks  ": [5, 6, 5]}
+    )
+
+    expected = pd.DataFrame(
+        {"Age": [20, 30], "Name": ["Alice", "Bob"], "Marks  ": [5, 6]}
+    )
+
+    result = drop_duplicates(original)
+
+    pd.testing.assert_frame_equal(result, expected)
+
+
+def test_fix_invalid_monthly_charges() -> None:
+    original = pd.DataFrame({"MonthlyCharges": [10, 20, -3]})
+
+    expected = pd.DataFrame({"MonthlyCharges": [10, 20, 15]})
+
+    result = fix_invalid_monthly_charges(original)
+    pd.testing.assert_frame_equal(result, expected)
